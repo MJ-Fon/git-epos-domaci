6405d50eb55859f91f64e4cf393d32624c88f7bb
Marija J <marija.jovicic.pn@gmail.com>
2021-01-31 12:58:47 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//S10E
+var s10e = document.getElementById('SS10E');
+s10e.onclick = function() {
+    console.log(64656 + " dinara");
+}
+//11 Pro W
+var prow = document.getElementById('pro11W');
+prow.onclick = function() {
+    console.log(164580 + " dinara");
+}
+
+//11 pro B
+var prob = document.getElementById('pro11B');
+prob.onclick = function() {
+    console.log(129313 + " dinara");
+}
+
+//X
+var X = document.getElementById('ipX');
+X.onclick = function() {
+    console.log(103450 + " dinara");
+}
+
+//S10
+var s10 = document.getElementById('SS10');
+s10.onclick = function() {
+    console.log(105800 + " dinara");
+}
+
+//S9
+var s9 = document.getElementById('ss9');
+s9.onclick = function() {
+    console.log(76412 + " dinara");
+}
